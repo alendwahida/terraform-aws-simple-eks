2012dc42a5f3b5e028ac7d243c9e6fcb3a1c53ae
Alend Wahida <alendwahida@gmail.com>
2022-04-28 09:51:48 +0700

    Update variable tf dan tfvars

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -59,9 +59,9 @@ resource "aws_eks_node_group" "awn-simple-eks-project-eks-node-group" {
 	]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = var.eks_desired_size
+    max_size     = var.eks_max_size
+    min_size     = var.eks_min_size
   }
 
   update_config {
